ac347dd65d6ae111d72f30d9dad2af5dd1e4482f
Dromediansk <pillar.mr@gmail.com>
2023-11-18 22:40:47 +0100

    Updating notes endpoint

--- server/src/prisma/migrations/20231118213912_init/migration.sql
+++ server/src/prisma/migrations/20231118213912_init/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "Note" (
     "id" TEXT NOT NULL,
     "text" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
 );
--- server/src/routes/notes/controllers.ts
+++ server/src/routes/notes/controllers.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { prisma } from "../../prisma/db";
+import { Prisma } from "@prisma/client";
 
 export const getAllNotes = async (req: Request, res: Response) => {
   try {
-    const notes = await prisma.note.findMany();
+    const notes: Prisma.NoteCreateInput[] = await prisma.note.findMany();
     res.status(200).json({ data: notes });
   } catch (error) {
     res.status(500).json({ message: "Unable to get notes" });
