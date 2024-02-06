1480450cdee9110ad00292ce4961fea7faff86e9
Dromediansk <pillar.mr@gmail.com>
2024-02-06 14:43:53 +0100

    adding transitions to menu buttons

--- client-nextjs/src/components/StickyNote.tsx
+++ client-nextjs/src/components/StickyNote.tsx
@@ -2,8 +2,8 @@
 import { deleteNoteInDb } from "@/services/notes";
 import { Note } from "@prisma/client";
 import { useRouter } from "next/navigation";
-import { FC } from "react";
-import { Menu } from "@headlessui/react";
+import { FC, Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
 import OptionsIcon from "./icons/OptionsIcon";
 import EditIcon from "./icons/EditIcon";
 import DeleteIcon from "./icons/DeleteIcon";
@@ -12,12 +12,14 @@ type StickyNoteProps = {
   note: Note;
 };
 
+const menuItemClassName =
+  "p-2 cursor-pointer flex gap-2 items-center transition-colors duration-300";
+
 const StickyNote: FC<StickyNoteProps> = ({ note }) => {
   const router = useRouter();
 
   const handleDeleteNote = async () => {
     try {
-      console.log("hi");
       await deleteNoteInDb(note.id);
       router.refresh();
     } catch (error) {
@@ -26,7 +28,7 @@ const StickyNote: FC<StickyNoteProps> = ({ note }) => {
   };
 
   return (
-    <div className="border border-gray-200 gap-4 shadow-sm rounded z-10 flex flex-col justify-between group">
+    <div className="gap-4 shadow-md rounded z-10 flex flex-col justify-between group">
       <p
         title={note.text}
         className="max-w-sm text-center m-4 mb-0 line-clamp-3"
@@ -38,23 +40,43 @@ const StickyNote: FC<StickyNoteProps> = ({ note }) => {
           <Menu.Button className="hidden group-hover:block">
             <OptionsIcon />
           </Menu.Button>
-          <Menu.Items className="absolute flex flex-col p-2 bg-slate-100 rounded shadow-md">
-            <Menu.Item>
-              <span className="py-1 px-2 cursor-pointer flex gap-2 items-center">
-                <EditIcon />
-                <span>Update</span>
-              </span>
-            </Menu.Item>
-            <Menu.Item>
-              <span
-                className="py-1 px-2 cursor-pointer text-red-500 flex gap-2 items-center"
-                onClick={handleDeleteNote}
-              >
-                <DeleteIcon />
-                <span>Delete</span>
-              </span>
-            </Menu.Item>
-          </Menu.Items>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute flex flex-col bg-slate-100 rounded shadow-md">
+              <Menu.Item>
+                {({ active }) => (
+                  <span
+                    className={`${menuItemClassName} ${
+                      active ? "bg-gray-200" : ""
+                    }`}
+                  >
+                    <EditIcon />
+                    <span>Update</span>
+                  </span>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <span
+                    className={`${menuItemClassName} text-red-500 ${
+                      active ? "bg-gray-200" : ""
+                    }`}
+                    onClick={handleDeleteNote}
+                  >
+                    <DeleteIcon />
+                    <span>Delete</span>
+                  </span>
+                )}
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
         </Menu>
       </footer>
     </div>
